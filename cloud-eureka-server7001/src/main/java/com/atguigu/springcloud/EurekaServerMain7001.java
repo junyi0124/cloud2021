9b0b8147a2901394e36cb8ce5978da0b24b5d25e
junyi <junyi0124@msn.com>
2021-03-29 21:01:26 +0800

    eureka finished

--- cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaServerMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaServerMain7001.java
@@ -2,10 +2,13 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurekaServerMain7001 {
+
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(EurekaServerMain7001.class, args);
     }
 }
