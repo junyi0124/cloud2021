84b47e2fa5db5fd88346aa635f6591981671683a
junyi <junyi0124@msn.com>
2021-03-29 15:43:24 +0800

    finish first day

--- cloud-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -2,20 +2,30 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.common.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@Slf4j
 public class OrderController {
 
     private static final String PAYMENT_URL = "http://localhost:8001";
 
+    @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/consumer/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
-        return restTemplate.postForObject(PAYMENT_URL +"", payment, CommonResult.class);
+        return restTemplate.postForObject(PAYMENT_URL +"/api/payment", payment, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/{id}")
+    public CommonResult getById(@PathVariable("id") long id) {
+        return restTemplate.getForObject(PAYMENT_URL +"/api/payment/" + id, CommonResult.class);
     }
 }
